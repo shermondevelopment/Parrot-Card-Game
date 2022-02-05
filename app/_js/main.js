9db545b06583fb652d13ae6138a27963ae82ef4b
Victor Shermon <victor804.gt@gmail.com>
2022-02-05 18:21:46 -0300

    rendering text on screen

--- app/_js/main.js
+++ app/_js/main.js
@@ -1,3 +1,17 @@
+let numberCards;
+let cardBox = document.querySelector('.main__cards');
+
 function startGame() {
-  let numberCards = prompt("Digite um número de cartas par entre 4 e 14")
+  numberCards = parseInt(prompt("Digite um número de cartas par entre 4 e 14"))
+  while(numberCards % 2 !== 0  || numberCards > 14 || numberCards <= 2){
+    this.answerValue = parseInt(prompt("Digite um número de cartas par entre 4 e 14"))
+   }
 }
+
+function showCards() {
+  for(let i = 0; i < numberCards; i++) {
+    cardBox.innerHTML += `<span>Olá</span>`
+  }
+}
+
+showCards()
